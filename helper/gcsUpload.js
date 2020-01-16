5ad6a1302f4676b73d601a69a92bd2c656440afd
japhendywijaya <jap_hendy_wijaya@yahoo.co.id>
2020-01-16 17:57:44 +0700

    ngecomment keyfilename di gcs upload

--- helper/gcsUpload.js
+++ helper/gcsUpload.js
@@ -5,7 +5,7 @@ module.exports = gcsUpload({
         fileSize: 1e6 // in bytes
       },
       gcsConfig: {
-        keyFilename: './expression-detector-75bc5c7a822d.json',
+        // keyFilename: './expression-detector-75bc5c7a822d.json',
         bucketName: 'ed-h8-storage'
       }
 })
